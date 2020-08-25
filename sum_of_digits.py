46e1b438e34cf4a38eb2f230bd01b5c50673c4fa
sivaprakashSP <sivaprakashsp@gmail.com>
2020-08-25 11:21:19 +0530

    Task for sum of digits

--- sum_of_digits.py
+++ sum_of_digits.py
@@ -9,11 +9,13 @@ for i in range(1,999+1):
     if len(str(result))==1:
         if result_sum==result:
             #print(i,end="\n")
-            lis.append(i)
+            j=str(i).zfill(4)
+            lis.append(j)
     elif len(str(result))==2:
         res=sum_of_digit(result)
         if result_sum==res:
             #print(i,end="\t") 
-            lis.append(i)
+            j=str(i).zfill(4)
+            lis.append(j)
 for i in range(len(lis)):
     print(lis[i],end=" ")
